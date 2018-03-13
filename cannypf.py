88c67e5439cd44ba27250637c921948dafe7d619
wm0.0 <ludlows@foxmail.com>
2018-03-13 12:42:06 -0400

    fixed bug cannypy

--- cannypf.py
+++ cannypf.py
@@ -33,13 +33,14 @@ class CannyPF(object):
         num_row, num_col = self.gray_img.shape
         # compute meaningful length
         meaningful_length = int(2.0 * np.log(num_row * num_col) / np.log(8) + 0.5)
+        print("meaningful_length", meaningful_length)
         angle = 2 * np.arctan(2 / meaningful_length)
         smth_img = cv2.GaussianBlur(self.gray_img, (self.gauss_size, self.gauss_size), 1.0)
         self.smth_img = smth_img
         # compute gradient map and orientation map
         gradient_map = np.zeros((num_row, num_col))
-        dx = cv2.Sobel(smth_img,cv2.CV_64F,1,0,ksize=3)
-        dy = cv2.Sobel(smth_img,cv2.CV_64F,0,1,ksize=3)
+        dx = cv2.Sobel(smth_img,cv2.CV_64F,1,0,ksize=3,scale=1, delta=0, borderType=cv2.BORDER_REPLICATE)
+        dy = cv2.Sobel(smth_img,cv2.CV_64F,0,1,ksize=3,scale=1, delta=0, borderType=cv2.BORDER_REPLICATE)
 
         # construct a histogram
         gray_levels = 255
@@ -57,31 +58,42 @@ class CannyPF(object):
                     gradient_map[ind_r, ind_c] = 0
         # gradient statistic
         num_p = np.sum(hist * (hist-1))
+        print(" N2 = ", num_p)
         #
         p_max = 1.0 / np.exp(np.log(num_p)/meaningful_length)
         p_min = 1.0 / np.exp(np.log(num_p)/np.sqrt(num_row * num_col))
-        prob = np.cumsum(hist) / total_num
+        print('p_max', p_max)
+        print('p_min', p_min)
+        print("hist[8*graylevels-1]= ", hist[gray_levels*8-1])
+        count = 0
+        prob = np.zeros((8*gray_levels))
+        for i in range(8*gray_levels-1, -1, -1):
+            count += hist[i]
+            prob[i] = count / total_num
+      
+        print("prob[8*255-1] = ", prob[8*255-1])
         # compute two threshold
         high_threshold = 0
         low_threshold = 1.3333
-        hist_length = hist.shape[0]
-
-        for i in range(hist_length):
-            p_cur = prob[-i-1]
+        
+        for i in range(8*gray_levels-1, -1,-1):
+            p_cur = prob[i]
             if p_cur > p_max:
-                high_threshold = hist_length - i - 1
+                high_threshold = i
                 break
-        for i in range(hist_length):
-            p_cur = prob[-i-1]
+        for i in range(8*gray_levels-1, -1,-1):
+            p_cur = prob[i]
             if p_cur > p_min:
-                low_threshold = hist_length - i - 1
+                low_threshold = i
                 break
         if low_threshold < 1.3333:
             low_threshold = 1.3333
         # visual meaningful high threshold
         high_threshold = np.sqrt(high_threshold * self.vm_grad)
+        print('low_threshold     high_threshold')
         print(low_threshold, high_threshold)
         return low_threshold, high_threshold
+        
     def comp_edge_map(self):
         """
         a wrapper for canny detector in OpenCV
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
     img = cv2.imread(img_path, 0)
     #print(img.shape)
     cannypf = CannyPF(3, 70, img)
-    vd = cannypf.comp_edge_map()
+    edgemap = cannypf.comp_edge_map()
+    cv2.imwrite("C:\\Users\\Administrator\\Downloads\\lena30py.jpg", edgemap)
 
 
 if __name__  == "__main__":
